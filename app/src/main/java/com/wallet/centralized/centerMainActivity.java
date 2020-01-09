1fd43b4e426250fa1305ce4faebba0971ba703ae
秦始皇在现代 <tiffary123@outlook.com>
2020-01-09 21:29:45 +0800

    添加获取屏幕尺寸 密度的代码

--- app/src/main/java/com/wallet/centralized/centerMainActivity.java
+++ app/src/main/java/com/wallet/centralized/centerMainActivity.java
@@ -3,13 +3,17 @@ package com.wallet.centralized;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -47,6 +51,25 @@ public class centerMainActivity extends AppCompatActivity implements OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_centermain);
+        Resources resources = this.getResources();
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int resourceId = resources.getIdentifier("navigation_bar_height","dimen", "android");
+        int navigation_bar_height = resources.getDimensionPixelSize(resourceId);
+        int width = dm.widthPixels;         // 屏幕宽度（像素）
+        int height = dm.heightPixels;       // 屏幕高度（像素）
+        float density = dm.density;         // 屏幕密度（0.75 / 1.0 / 1.5）
+        int densityDpi = dm.densityDpi;     // 屏幕密度dpi（120 / 160 / 240）
+        // 屏幕宽度算法:屏幕宽度（像素）/屏幕密度
+        int screenWidth = (int) (width / density);  // 屏幕宽度(dp)
+        int screenHeight = (int) (height / density);// 屏幕高度(dp)
+        Log.d("h_bl", "屏幕宽度（像素）：" + width);
+        Log.d("h_bl", "屏幕高度（像素）：" + height);
+        Log.d("h_bl", "屏幕密度（0.75 / 1.0 / 1.5）：" + density);
+        Log.d("h_bl", "屏幕密度dpi（120 / 160 / 240）：" + densityDpi);
+        Log.d("h_bl", "屏幕宽度（dp）：" + screenWidth);
+        Log.d("h_bl", "屏幕高度（dp）：" + screenHeight);
         Data.settype("centermain");
         Data.setcontext(centerMainActivity.this);
         et_user=(EditText) findViewById(R.id.username);
